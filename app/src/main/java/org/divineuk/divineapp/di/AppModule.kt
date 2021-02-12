300751d43f7f71ccbb39d09ac29547a7c69600b4
Sabin Jose <sabinjose90@gmail.com>
2021-02-12 22:19:24 +0000

    Bumped Gradle Dependency Versions

--- app/src/main/java/org/divineuk/divineapp/di/AppModule.kt
+++ app/src/main/java/org/divineuk/divineapp/di/AppModule.kt
@@ -5,7 +5,7 @@ import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import io.gsonfire.GsonFireBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -16,7 +16,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
